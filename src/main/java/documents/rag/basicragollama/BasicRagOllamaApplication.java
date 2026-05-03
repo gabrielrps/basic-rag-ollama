0d2cea8660c4c2a2d45acbfea100373ad544060c
Gabriel Rezende de Paula Souza <gabriel.rezendeps@gmaill.com>
2026-05-03 10:00:29 +0200

    first commit

---
+++ src/main/java/documents/rag/basicragollama/BasicRagOllamaApplication.java
@@ -0,0 +1,13 @@
+package documents.rag.basicragollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRagOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRagOllamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/documents/rag/basicragollama/BasicRagOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package documents.rag.basicragollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRagOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
